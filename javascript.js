26d71a3b0bff2f8a5648394f72afda6cc8c68ce6
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-05-08 17:32:01 +0200

    Add hover effect on grid that changes the color
    Create new variable with querySelectorAll to all the divs that are a
    child element of container.
    Iterate over each div then with forEach and run an EventListener with
    mouseover on each div that changes the background color of the div to
    black

--- javascript.js
+++ javascript.js
@@ -4,3 +4,12 @@ for (let i = 1; i < 257; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
 }
+
+const divChanges = document.querySelectorAll(".container>div");
+
+    divChanges.forEach((divChange) => {
+        divChange.addEventListener("mouseover", () => {
+            divChange.style.cssText = "background: black";
+        }); 
+    });
+   
