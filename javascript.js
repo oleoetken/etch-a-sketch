1100472e1f9acb8b441081da390e60ffcbbc489b
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-05-13 17:45:22 +0200

    Continue the task and add button at the top
    Button for input for user to declare size of the grid.
    Set button in new html div to put above grid.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,27 @@
 const container = document.querySelector(".container");
 
+const button = document.querySelector(".button");
+const btn = document.createElement("button");
+btn.textContent = "Set squares";
+
+btn.addEventListener("click", () => {
+    let squareButton = prompt("Set number of squares");
+
+    if (squareButton > 100) {
+        return alert("Please choose a smaller number");
+    } 
+    else {
+        console.log(document.querySelectorAll("grid").length);
+        document.querySelectorAll(".grid").forEach(el => el.remove());
+        
+    }
+});
+button.appendChild(btn);
+
+
 for (let i = 1; i < 257; i++) {
     const div = document.createElement("div");
+    div.classList.add = "grid";
     container.appendChild(div);
 }
 
