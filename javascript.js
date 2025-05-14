32b72a0cd48025de32ae28c876785de6c3d4e40d
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-05-14 14:04:29 +0200

    Create creatGrid function
    Use creatGrid function so it can be used in prompt to create the grid
    size the user inputs. Needs more work, can createGrid already but can't
    delete old grid. Also still no idea how to keep the grid the same size
    at 960px

--- javascript.js
+++ javascript.js
@@ -1,8 +1,30 @@
 const container = document.querySelector(".container");
 
+
 const button = document.querySelector(".button");
 const btn = document.createElement("button");
 btn.textContent = "Set squares";
+
+function createGrid(squareNumber) {
+
+    for (let i = 0; i < squareNumber * squareNumber; i++) {
+        const div = document.createElement("div");
+        div.classList.add = "grid";
+        container.appendChild(div);
+    }
+    
+    const divChanges = document.querySelectorAll(".container>div");
+    
+        divChanges.forEach((divChange) => {
+            divChange.addEventListener("mouseover", () => {
+                divChange.style.cssText = "background: black";
+            }); 
+        });
+
+}
+
+createGrid(16);
+
 
 btn.addEventListener("click", () => {
     let squareButton = prompt("Set number of squares");
@@ -11,25 +33,11 @@ btn.addEventListener("click", () => {
         return alert("Please choose a smaller number");
     } 
     else {
-        console.log(document.querySelectorAll("grid").length);
-        document.querySelectorAll(".grid").forEach(el => el.remove());
+        createGrid(squareButton);
         
     }
 });
 button.appendChild(btn);
 
 
-for (let i = 1; i < 257; i++) {
-    const div = document.createElement("div");
-    div.classList.add = "grid";
-    container.appendChild(div);
-}
-
-const divChanges = document.querySelectorAll(".container>div");
-
-    divChanges.forEach((divChange) => {
-        divChange.addEventListener("mouseover", () => {
-            divChange.style.cssText = "background: black";
-        }); 
-    });
    
