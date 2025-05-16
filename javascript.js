dde731a8bbe04eabd758ec1aae3600ad6a3de444
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-05-16 14:00:06 +0200

    Finally add function to remove old grid
    Use innerHTML to make the createGrid function to remove the old grid
    when a new one is prompted by the user by overwriting the HTML.
    Fix some px issues that made the grid not be the right size, now always
    has same px in height and width.
    Add the updateOpactiy and randomRGB function to now also make it so that
    the opacity is changed by 0.1 every mouseover so the grid-item gets
    darker and the random color gets more clear.
    randomRGB function creates that random color every mouseover

--- javascript.js
+++ javascript.js
@@ -1,17 +1,9 @@
 const container = document.querySelector(".container");
 
-function changeGridSize() {
-    let squares = prompt("Input a number between 1 and 100");
-    if (squares > 100 || squares < 1) {
-        alert("Unable to create this grid");
-    }
-    else {
-        createGrid(squares);
-    }
-}
-
 function createGrid(size) {
-    let gridCellSize = 1000 / size;
+    container.innerHTML = "";
+
+    let gridCellSize = 960 / size;
     for (let i = 0; i < size * size; i++) {
         const gridCell = document.createElement("div");
         gridCell.classList.add("grid-item");
@@ -20,11 +12,40 @@ function createGrid(size) {
         container.appendChild(gridCell);
 
         gridCell.addEventListener("mouseover", () => {
-            gridCell.style.backgroundColor = "black";
+            gridCell.style.backgroundColor = randomRGB();
+            updateOpacity(gridCell);
         })
     }
 }
 
-createGrid(100);
+createGrid(16);
 
-   
+function updateOpacity(gridCell) {
+    let currentOpacity = parseFloat(gridCell.style.opacity) || 0.1;
+
+    currentOpacity += 0.1;
+    if (currentOpacity > 1) {
+        currentOpacity = 1;
+    }
+    gridCell.style.opacity = currentOpacity;
+}
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const button = document.querySelector(".set-grid");
+
+button.addEventListener("click", () => {
+    let squares = parseInt(prompt("Input a number between 1 and 100"));
+
+    if (squares > 100 || squares < 1) {
+        alert("Unable to create this grid");
+    }
+    else {
+        createGrid(squares);
+    }
+})
